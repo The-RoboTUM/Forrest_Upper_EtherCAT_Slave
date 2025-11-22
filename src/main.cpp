1f29fca34be5d7f02f402e614312a070c8ca7301
Paul Timke Contreras <ptimkec@live.com>
2025-11-22 13:47:26 +0100

    Add logging and example on main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include "FreeRTOS.h"
-#include "portable.h"
-#include "projdefs.h"
 #include "task.h"
+#include "logging.h"
 
 static void test_task(void* pvParams) {
     (void)pvParams;
 
+    int i = 0;
     while (1) {
-        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        logger_println("Main", "Hello %d", i);
+        i++;
     }
 }
 
 int main(void)
 {
+    logger_init();
+
     // Create RTOS tasks
     xTaskCreate(test_task, "test_task", 256, NULL, tskIDLE_PRIORITY + 1, NULL);
 
